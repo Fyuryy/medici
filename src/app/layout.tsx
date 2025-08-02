adf575fb91f44e95415883d15e3973b849cbeade
Fyuryy <plagouveia@gmail.com>
2025-08-02 22:27:05 +0200

    added favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,12 +3,21 @@ import { ReactNode } from 'react'
 import { SupabaseProvider } from './providers'
 import { Roboto } from 'next/font/google'
 
-
 const roboto = Roboto({
   subsets: ['latin'],
   weight: ['400', '700'],
   variable: '--font-roboto',
 })
+
+// app/layout.js
+export const metadata = {
+  title: 'Medici Studio',
+  icons: {
+    icon: '/LOGO_MEDICI_BLANC.png', // path to your image in public/
+    shortcut: '/LOGO_MEDICI_BLANC.png', // for older browsers
+    apple: '/LOGO_MEDICI_BLANC.png', // for iOS home-screen
+  },
+}
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
